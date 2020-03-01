fe0450df22de81813e721b7773e6fc687c664481
minmin233 <yyangshumin@163.com>
2020-03-01 18:10:39 +0800

    image组件

---
+++ pages/image/image.js
@@ -0,0 +1,84 @@
+// pages/image/image.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imagePath:''
+  },
+  handlePic(){
+    //系统API让用户在相册中选择图片或者拍照
+    wx.chooseImage({
+      success: (res)=> {
+        console.log(res)
+        //1.取出路径
+        const path = res.tempFilePaths[0]
+        //2.设置imagePath
+        //回调函数中丢失当前函数的上下文对象，this:undefined,要用箭头函数
+        this.setData({
+          imagePath:path
+        })
+      },
+    })
+  },
+  handleImageLoad(){
+    console.log('图片加载完成')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
