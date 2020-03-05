957ecf4c2ea9e2cd082f335a7c061b0d655ab85a
minmin233 <yyangshumin@163.com>
2020-03-05 14:33:29 +0800

    事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,8 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles:['衣服','裤子','鞋子']
   },
+  handleItemClick(event){
+    console.log(event)
+    const dataset = event.currentTarget.dataset;
+    const title=dataset.item;
+    const index=dataset.index;
+    console.log(title,index)
+  },
+  handleTouchStart(){
+    console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
+  },
+
+  //-------事件冒泡和事件捕获
+  handleCaptureView1(){
+    console.log('handleCaptureView1')
+  },
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
+  },
+  
+
 
   /**
    * 生命周期函数--监听页面加载
